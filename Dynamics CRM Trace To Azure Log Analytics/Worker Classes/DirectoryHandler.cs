c7d50312889e466332d15afabae440125269f3c8
Colton <colton.r.lathrop@gmail.com>
2018-07-26 22:11:17 -0500

    Add files via upload

---
+++ Dynamics CRM Trace To Azure Log Analytics/Worker Classes/DirectoryHandler.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics_CRM_Trace_To_Azure_Log_Analytics
+{
+    class DirectoryHandler
+    {
+        //Temporary Static Variables for POC.
+        string _directory = @"C:\Program Files\Microsoft Dynamics CRM\Trace";
+
+        private bool VerifyTraceDirectory(string directory)
+        {
+            return Directory.Exists(directory);
+        }
+
+        private IEnumerable<string> RetrieveFiles(string directory)
+        {
+            return Directory.GetFiles(directory);
+        }
+
+        private List<FileInfo> BuildFileObjects(string directory)
+        {
+            if (VerifyTraceDirectory(directory) != true)
+            {
+                throw new FileNotFoundException();
+            }
+            else
+            {
+                var fileObjects = new List<FileInfo>();
+                var fileStrings = RetrieveFiles(directory);
+                foreach (string fileName in fileStrings)
+                {
+                    fileObjects.Add(new FileInfo(fileName));
+                }
+                return fileObjects;
+            }
+        }
+
+        private List<FileInfo> SortFiles(List<FileInfo> files)
+        {
+            List<FileInfo> sorted = new List<FileInfo>(5);
+            foreach (var x in files)
+            {
+                if (sorted.Count < 5)
+                {
+                    sorted.Add(x);
+                    continue;
+                }
+                var index = -1;
+                foreach (var z in sorted.ToList())
+                {
+                    if (x.LastWriteTimeUtc < z.LastWriteTimeUtc)
+                    {
+                        index = sorted.IndexOf(z);
+                    }
+                }
+                if (index != -1)
+                {
+                    sorted[index] = x;
+                }
+            }
+            return sorted;
+        }
+
+        private List<FileInfo> PullFive()
+        {
+            List<FileInfo> files = BuildFileObjects(_directory);
+            var sorted_files = SortFiles(files);
+            return sorted_files;
+        }
+
+        private byte[] ConvertToBytes(string target)
+        {
+            return new UTF8Encoding(true).GetBytes(target);
+        }
+
+        public void Debug()
+        {
+            var z = File.Create(@"C:\Users\colathro\source\repos\Dynamics CRM Trace To Azure Log Analytics\Dynamics CRM Trace To Azure Log Analytics\test.txt");
+            var x = PullFive();
+            foreach (FileInfo test in x)
+            {
+                byte[] f = ConvertToBytes(test.Name + test.LastWriteTimeUtc.ToString() + Environment.NewLine);
+                z.Write(f, 0, f.Length);
+            }
+        }
+    }
+}
---
+++ Dynamics CRM Trace To Azure Log Analytics/Worker Classes/TraceHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dynamics_CRM_Trace_To_Azure_Log_Analytics.Worker_Classes
+{
+    class TraceHandler
+    {
+    }
+}
